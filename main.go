5a0538efdcaaa2c80856f4b6d0f88d9d2e8351ff
Shinyzenith <aakashsensharma@gmail.com>
2022-08-17 20:32:20 +0530

    feat(devbox): Create rootfs based on alpine linux
    Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"context"
+
 	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/namespaces"
+	"github.com/containerd/containerd/oci"
 )
 
 func main() {
-	client, err := containerd.New("./test.sock")
+	client, err := containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	ctx := namespaces.WithNamespace(context.Background(), "Devbox")
+	image, err := client.Pull(ctx, "docker.io/library/alpine:latest", containerd.WithPullUnpack)
 	if err != nil {
 		panic(err)
 	}
 
-	defer client.Close()
+	container, err := client.NewContainer(
+		ctx,
+		"replace_me_later",
+		// By using different id's we can reuse them across containers
+		containerd.WithNewSnapshot("replace_me_later", image),
+		containerd.WithNewSpec(oci.WithImageConfig(image)),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer container.Delete(ctx, containerd.WithSnapshotCleanup)
 }
--- main.md
+++
@@ -1,9 +0,0 @@
-# Notes:
-- ttrpc for custom gRPC communication
-- Do we wanna use dBus over gRpc?
-- Isn't containerd too heavy for the project?
-	- Custom namespace based containers?
-
-# Example:
-- https://github.com/containerd/containerd/blob/main/docs/getting-started.md
-
