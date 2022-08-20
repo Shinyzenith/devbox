6db79933454cc1dbf4e1a53aa258a2b9925d8eca
Shinyzenith <aakashsensharma@gmail.com>
2022-08-20 16:43:52 +0530

    feat(devbox): Parse create flags-> tag, image, name
    Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

--- main.go
+++ main.go
@@ -4,22 +4,44 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/alecthomas/kong"
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/namespaces"
 	"github.com/containerd/containerd/oci"
 )
 
-func main() {
-	ParseFlags()
-	ctx := namespaces.WithNamespace(context.Background(), "Devbox")
+var CLI struct {
+	Create struct {
+		Image string `arg:"" help:"Set the container image" type:"string" name:"image"`
+		Tag   string `arg:"" help:"Set the image tag." type:"string" name:"tag"`
 
+		Name string `arg:"" name:"name" help:"Set the container name." type:"path"`
+	} `cmd:"" help:"Create a dev container."`
+}
+
+var CFG struct {
+	image_name     string
+	image_tag      string
+	container_name string
+}
+
+func main() {
+	kong_ctx := kong.Parse(&CLI, kong.Name("Devbox"), kong.Description("Easy, simple, ephemeral dev containers"), kong.UsageOnError(), kong.ConfigureHelp(kong.HelpOptions{Compact: true, Summary: true}))
+	switch kong_ctx.Command() {
+	case "create <image> <tag> <name>":
+		CFG.image_name = CLI.Create.Image
+		CFG.image_tag = CLI.Create.Tag
+		CFG.container_name = CLI.Create.Name
+	}
+
+	ctx := namespaces.WithNamespace(context.Background(), "Devbox")
 	client, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
 		panic(err)
 	}
 	defer client.Close()
 
-	image, err := GetImage(ctx, client, "alpine", "latest")
+	image, err := GetImage(ctx, client, CFG.image_name, CFG.image_tag)
 	if err != nil {
 		panic(err)
 	}
